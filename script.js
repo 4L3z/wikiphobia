027e8beb0a24cf6a59cd9081b4704e5c35c89391
4L3z <palaciosalejandro735@gmail.com>
2024-08-19 22:13:04 -0300

    home page in progress. part 1

---
+++ script.js
@@ -0,0 +1,25 @@
+function showScreamer() {
+    Swal.fire({
+      title: '¡Sorpresa!',
+      text: 'No hay ningún screamer :)',
+      icon: 'warning',
+      showCancelButton: false,
+      confirmButtonText: 'OK',
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Mostrar el screamer
+        const screamer = document.createElement('video');
+        screamer.src = './src/screamer.mp4'; // Reemplaza con la ruta de tu video
+        screamer.autoplay = true;
+        screamer.volume = '100';
+        screamer.style.width = '100%';
+        screamer.style.height = '100%';
+        document.body.appendChild(screamer);
+      }
+    });
+  }
+
+
