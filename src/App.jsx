176cc6bef6ff58ebef722a9bae73828c5753baf6
4L3z <palaciosalejandro735@gmail.com>
2024-09-17 10:11:26 -0300

    Nivel 3, Navbar nuevo en la pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,38 @@ import { Homepage } from './components/Homepage';
 import Nivel1 from './components/Objects/Nivel1';
 import Nivel2 from './components/Objects/Nivel2';
 import Nivel3 from './components/Objects/Nivel3';
-import {SideBar} from './components/Sidebar';
-import {Maps} from './components/Maps/Maps';
+import { Maps } from './components/Maps/Maps';
 import Ghosts from './components/Ghost/Ghosts';
 import Objects from './components/Objects/Objects';
 import { NotFound } from './components/404/NotFound';
-
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <Router>
       <Routes>
-      <Route
+        <Route
           path="/"
           element={
             <>
-              <SideBar />
-              <Homepage />
-              <Maps />
-              <Ghosts />
-              <Objects />
+              <Navbar />
+              <section id="homepage">
+                <Homepage />
+              </section>
+              <section id="maps">
+                <Maps />
+              </section>
+              <section id="ghosts">
+                <Ghosts />
+              </section>
+              <section id="objects">
+                <Objects />
+              </section>
             </>
           }
         />
         <Route path="/nivel1" element={<Nivel1 />} />
         <Route path="/nivel2" element={<Nivel2 />} />
         <Route path="/nivel3" element={<Nivel3 />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
