c3df20a2916a8c966f39a1126d75251f21cb954e
4L3z <palaciosalejandro735@gmail.com>
2024-09-26 10:58:29 -0300

    Requisitos change

--- src/components/Requisitos/Requisitos.jsx
+++ src/components/Requisitos/Requisitos.jsx
@@ -9,11 +9,11 @@ const Requisitos = () => {
           <h2>MÍNIMOS</h2>
           <ul>
             <li>Sistema Operativo: Windows 10 de 64 bits</li>
-            <li>Procesador: Intel Core i5-4590 o AMD FX 8350</li>
+            <li>Procesador: Intel Core i5-4590 o AMD Ryzen 5 2600</li>
             <li>Memoria: 8 GB de RAM</li>
             <li>Gráficos: NVIDIA GTX 970 / AMD Radeon R9 290</li>
             <li>DirectX: Versión 11</li>
-            <li>Almacenamiento: 15 GB de espacio disponible</li>
+            <li>Almacenamiento: 21 GB de espacio disponible</li>
           </ul>
         </div>
         <div className="vs">
@@ -27,7 +27,7 @@ const Requisitos = () => {
             <li>Memoria: 16 GB de RAM</li>
             <li>Gráficos: NVIDIA RTX 2060 / AMD RX 5700</li>
             <li>DirectX: Versión 12</li>
-            <li>Almacenamiento: 15 GB de espacio disponible</li>
+            <li>Almacenamiento: 21 GB de espacio disponible</li>
           </ul>
         </div>
       </div>
